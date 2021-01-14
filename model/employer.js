cb3e8759349dff7c33ee47ffcb1f610153cdae79
Trần  Khoa <trankhoa@Trans-MacBook-Pro.local>
2021-01-14 09:12:42 +0700

    rebase

--- model/employer.js
+++ model/employer.js
@@ -1,26 +1,6 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-const Schema = mongoose.Schema;
-
-const Company = new Schema({
-  // coTypeId: { type: Schema.Types.ObjectId, required: true },
-  coName: { type: String, required: true },
-  coEmail: { type: String, required: true },
-  coPassword: { type: String, required: true },
-  coPhone: { type: String, required: true },
-  coAddress: { type: String, required: true },
-  coTaxCode: { type: String, required: true },
-  coLogo: { type: String, required: true, default: null },
-  coDescription: { type: String, required: true },
-  coStatus: { type: String, required: true, default: "Pending" },
-  coTypeId: { type: Schema.Types.ObjectId, ref: "CompanyType", required: true },
-  createdAt: { type: Date, min: "1990-01-01", max: "2099-01-01" },
-  updatedAt: {
-=======
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
 // empTypeId;
 // empName;
 // empEmail;
@@ -67,20 +47,13 @@ const Employer = new Schema({
     default: null,
   },
   updatedInfoAt: {
->>>>>>> 0c210c6 (Employer)
     type: Date,
     min: "1990-01-01",
     max: "2099-01-01",
     default: null,
   },
-<<<<<<< HEAD
+
   confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
   updatedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
 });
-
-module.exports = Company;
-=======
-});
-
 module.exports = Employer;
->>>>>>> 0c210c6 (Employer)
