1efb54a988204e12e0b56f4a0ab7a9d194c8ebaa
Trần  Khoa <trankhoa@Trans-MacBook-Pro.local>
2021-01-14 09:20:26 +0700

    employer.js

--- model/employer.js
+++ model/employer.js
@@ -35,54 +35,6 @@ const Employer = new Schema({
     required: true,
   },
   createdAt: { type: Date, min: "1990-01-01", max: "2099-01-01" },
-  confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
-  confirmedAt: {
-    type: Date,
-    min: "1990-01-01",
-    max: "2099-01-01",
-    default: null,
-  },
-  updatedPasswordAt: {
-    type: Date,
-    min: "1990-01-01",
-    max: "2099-01-01",
-    default: null,
-  },
-  updatedInfoAt: {
-
-const Schema = mongoose.Schema;
-// empTypeId;
-// empName;
-// empEmail;
-// empPassword;
-// empPhone;
-// empAddress;
-// empTaxcode;
-// empDescription;
-// empLogo;
-// createdAt;
-// updatedPasswordAt;
-// updatedInfoAt;
-// confirmedBy;
-// confirmedAt;
-
-const Employer = new Schema({
-  // coTypeId: { type: Schema.Types.ObjectId, required: true },
-  empName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  empPhone: { type: String, required: true },
-  empAddress: { type: String, default: null },
-  empTaxCode: { type: String, default: null },
-  empLogo: { type: String, default: null },
-  empDescription: { type: String, default: null },
-  empStatus: { type: String, required: true, default: "Pending" },
-  empTypeId: {
-    type: Schema.Types.ObjectId,
-    ref: "EmpType",
-    required: true,
-  },
-  createdAt: { type: Date, min: "1990-01-01", max: "2099-01-01" },
 
   confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
   confirmedAt: {
@@ -91,11 +43,9 @@ const Employer = new Schema({
     max: "2099-01-01",
     default: null,
   },
-  updatedPasswordAt: {
+  updatedPasswordAt: {},
 
   updatedAt: {
-
-
     type: Date,
     min: "1990-01-01",
     max: "2099-01-01",
@@ -108,20 +58,6 @@ const Employer = new Schema({
     max: "2099-01-01",
     default: null,
   },
-
-
 });
 
 module.exports = Employer;
-
-
-  confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
-  updatedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
-});
-
-module.exports = Employer;
-
-
-module.exports = Company;
-
-
