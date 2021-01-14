57e5bd24bf4cf564f79f7f0d881ec170e77376aa
Trần  Khoa <trankhoa@Trans-MacBook-Pro.local>
2021-01-14 07:07:22 +0700

    Employer

--- controller/employer.controller.js
+++ controller/employer.controller.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const companyService = require("../service/company.service");
 
 let registerCompany = async (req, res) => {
@@ -36,4 +37,93 @@ module.exports = {
   registerCompany,
   getAllCompanies,
   updateCompany,
+=======
+const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_LIFE } = require("../global/global");
+const jwtHelpers = require("../helpers/jwt.helpers");
+const employerService = require("../service/employer.service");
+
+// empTypeId;
+// empId;
+// empName;
+// empEmail;
+// empPassword;
+// empPhone;
+// empAddress;
+// empTaxcode;
+// empDescription;
+// empLogo;
+// registeredAt;
+// updatedPasswordAt;
+// updatedInfoAt;
+
+let register = async (req, res, next) => {
+  let {
+    empTypeId,
+    empName,
+    empEmail,
+    empPassword,
+    empPhone,
+    empAddress,
+    empTaxcode,
+    empDescription,
+    empLogo,
+  } = req.body;
+  const registerResult = await employerService.userRegister({
+    empTypeId,
+    empName,
+    empEmail,
+    empPassword,
+    empPhone,
+    empAddress,
+    empTaxcode,
+    empDescription,
+    empLogo,
+  });
+  console.log(registerResult.code);
+  res.status(registerResult.code).send({
+    message: registerResult.message,
+  });
+};
+
+let login = async (req, res, next) => {
+  let { email, password } = req.body;
+  const loginResult = await employerService.login({
+    email,
+    password,
+  });
+  const accessToken = await jwtHelpers.generateToken(
+    email,
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_LIFE
+  );
+  res.status(loginResult.code).send({
+    message: loginResult.message,
+    userId: loginResult.id,
+    accessToken: accessToken,
+  });
+};
+
+let updatePassword = async (req, res, next) => {
+  let { email, password, newPassword } = req.query;
+  console.log("Update");
+  console.log(email, typeof password, typeof newPassword);
+  let newPasswordResult = await employerService.updatePassword({
+    email,
+    password,
+    newPassword,
+  });
+  // console.log(newPasswordResult);
+  res.status(newPasswordResult.code).send(newPasswordResult.message);
+};
+
+let getAllQuestions = (req, res, next) => {
+  res.send("Getting all questions...");
+};
+
+module.exports = {
+  register,
+  login,
+  getAllQuestions,
+  updatePassword,
+>>>>>>> 0c210c6 (Employer)
 };
--- model/employer.js
+++ model/employer.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+<<<<<<< HEAD
 const Schema = mongoose.Schema;
 
 const Company = new Schema({
@@ -15,13 +16,71 @@ const Company = new Schema({
   coTypeId: { type: Schema.Types.ObjectId, ref: "CompanyType", required: true },
   createdAt: { type: Date, min: "1990-01-01", max: "2099-01-01" },
   updatedAt: {
+=======
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+// empTypeId;
+// empName;
+// empEmail;
+// empPassword;
+// empPhone;
+// empAddress;
+// empTaxcode;
+// empDescription;
+// empLogo;
+// createdAt;
+// updatedPasswordAt;
+// updatedInfoAt;
+// confirmedBy;
+// confirmedAt;
+
+const Employer = new Schema({
+  // coTypeId: { type: Schema.Types.ObjectId, required: true },
+  empName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  empPhone: { type: String, required: true },
+  empAddress: { type: String, default: null },
+  empTaxCode: { type: String, default: null },
+  empLogo: { type: String, default: null },
+  empDescription: { type: String, default: null },
+  empStatus: { type: String, required: true, default: "Pending" },
+  empTypeId: {
+    type: Schema.Types.ObjectId,
+    ref: "EmpType",
+    required: true,
+  },
+  createdAt: { type: Date, min: "1990-01-01", max: "2099-01-01" },
+  confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
+  confirmedAt: {
     type: Date,
     min: "1990-01-01",
     max: "2099-01-01",
     default: null,
   },
+  updatedPasswordAt: {
+    type: Date,
+    min: "1990-01-01",
+    max: "2099-01-01",
+    default: null,
+  },
+  updatedInfoAt: {
+>>>>>>> 0c210c6 (Employer)
+    type: Date,
+    min: "1990-01-01",
+    max: "2099-01-01",
+    default: null,
+  },
+<<<<<<< HEAD
   confirmedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
   updatedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
 });
 
 module.exports = Company;
+=======
+});
+
+module.exports = Employer;
+>>>>>>> 0c210c6 (Employer)
