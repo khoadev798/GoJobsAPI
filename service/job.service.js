15765289376c1156362e70e9f670650b13c702a0
khoadev798 <trankhoa@Trans-MacBook-Pro.local>
2021-03-17 23:04:33 +0700

    Privatefile.json

--- service/job.service.js
+++ service/job.service.js
@@ -11,7 +11,8 @@ const FollowModel = mongoose.model("Follow", Follow);
 const util = require("../util/data.util");
 const admin = require("firebase-admin");
 const fcm = require("fcm-notification");
-const FCM = new fcm("D:/DuLieu/Duan2/GoJobsAPI/privatefile.json");
+const path = require("path");
+const FCM = new fcm(path.join(__dirname, "../privatefile.json"));
 const Notification = require("../model/notification");
 const NotificationModel = mongoose.model("Notification", Notification);
 
@@ -24,35 +25,34 @@ let createNewJob = async (job) => {
   let jobInstance = new JobModel(job);
   await jobInstance.save({ session: session });
   let isFollowExistedResult = await isFollowExisted(job);
-  if(isFollowExistedResult.code == 200){   
-
+  if (isFollowExistedResult.code == 200) {
     let tokenlists = isFollowExistedResult.follow;
     let Tokens = [];
     let flcIds = [];
-  
-    tokenlists.forEach((token) =>{
-        Tokens = Tokens.concat(token.tokenDeviceWithFlc);
-        flcIds = flcIds.concat(token.flcId);
-      });
-      let endTokens = await Promise.all(Tokens).then((values) =>{
-        return values;
-      });
-      let endFlcIds= await Promise.all(flcIds).then((values)=>{
-        return values;
-      });
 
-      let notification = {
-        flcId: endFlcIds,
-        jobId: jobInstance._id,
-        createdAt: new Date()
-      }
-      let notificationInstance = new NotificationModel(notification);
-       await notificationInstance.save(notification, (err, doc)=>{
-         if(err) handleError(err);
-         return console.log(doc);
-       });
+    tokenlists.forEach((token) => {
+      Tokens = Tokens.concat(token.tokenDeviceWithFlc);
+      flcIds = flcIds.concat(token.flcId);
+    });
+    let endTokens = await Promise.all(Tokens).then((values) => {
+      return values;
+    });
+    let endFlcIds = await Promise.all(flcIds).then((values) => {
+      return values;
+    });
 
-      console.log("toekn:", tokenlists);
+    let notification = {
+      flcId: endFlcIds,
+      jobId: jobInstance._id,
+      createdAt: new Date(),
+    };
+    let notificationInstance = new NotificationModel(notification);
+    await notificationInstance.save(notification, (err, doc) => {
+      if (err) handleError(err);
+      return console.log(doc);
+    });
+
+    console.log("toekn:", tokenlists);
     // var message = {
     //   data: {
     //     score: '850',
@@ -69,22 +69,21 @@ let createNewJob = async (job) => {
     //     }else {
     //         console.log('response-----', response);
     //     }
-     
+
     // })
-  };
+  }
 
   console.log("new jobs: ", jobInstance);
   await session.commitTransaction();
   session.endSession();
-  return { code: GLOBAL.SUCCESS_CODE, message: "Tao job moi thanh cong"};
+  return { code: GLOBAL.SUCCESS_CODE, message: "Tao job moi thanh cong" };
 };
 
-let isFollowExisted = async(job) =>{
-
+let isFollowExisted = async (job) => {
   let found = await FollowModel.find(
-    { empId: job.empId},
+    { empId: job.empId },
     "tokenDeviceWithFlc flcId",
-    (err, doc) =>{
+    (err, doc) => {
       if (err) return handleError(err);
       return doc;
     }
@@ -94,15 +93,15 @@ let isFollowExisted = async(job) =>{
     return {
       code: GLOBAL.NOT_FOUND_CODE,
       message: "Follow not found!",
-    }
-  }else{
+    };
+  } else {
     return {
       code: GLOBAL.SUCCESS_CODE,
-      message:"find success!",
+      message: "find success!",
       follow: found,
-    }
+    };
   }
-}
+};
 
 let getAllJobs = async () => {
   let jobs = [];
@@ -133,7 +132,8 @@ let getAllJobsAndEmployerInfo = async () => {
         { _id: job._doc.empId },
         "_id empName empEmail empStatus"
       ).exec();
-      queryList.push(query);gi 
+      queryList.push(query);
+      gi;
     });
     let employers = await Promise.all(queryList).then((values) => {
       return values;
@@ -201,7 +201,6 @@ let jobPagination = async (pagination) => {
 };
 
 let jobPaginationWithTime = async (pagination) => {
-
   let jobsWithConditions = await JobModel.find(
     {},
     "_id empId jobTitle jobDescription jobSalaryPerHour jobSalaryPerDay jobSalaryPerWeek jobSalaryAfterDone experienceRequired jobField jobStart jobEnd jobPublishDate jobStatus jobHeadCount",
