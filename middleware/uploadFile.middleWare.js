6fc2dbc5514fb3b9e9cf113518b5976a02bffce7
khoadev798 <trankhoa@Trans-MacBook-Pro.local>
2021-04-03 14:53:40 +0700

    Update hinh cho Emp

--- middleware/uploadFile.middleWare.js
+++ middleware/uploadFile.middleWare.js
@@ -1,29 +1,25 @@
-const path = require('path');
-const formidable = require('formidable');
-const fs = require('fs');
-const util = require('util');
+const path = require("path");
+const formidable = require("formidable");
+const fs = require("fs");
+const util = require("util");
 
 let uploadFile = (req, res, next) => {
- 
-    const form = new formidable.IncomingForm();
-      form.parse(req,  (err, field, file) =>{
-     
-      let oldPath = file.empLogo.path;
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, field, file) => {
+    let oldPath = file.empLogo.path;
     //   console.log(field);
-    let  newPath = path.join(__dirname,"../public/uploads", file.empLogo.name +".jpg");
-    fs.copyFile(oldPath, newPath, (err)=>{
-       
-        let project = 1;
-        res.end(util.inspect(project, {field: field, empLogo: newPath}));
-        // next();
+    let newPath = path.join(
+      __dirname,
+      "../public/uploads",
+      file.empLogo.name + ".jpg"
+    );
+    fs.copyFile(oldPath, newPath, (err) => {
+      req.empLogo = newPath;
+      req.fields = { ...field };
+      console.log(req.fields);
+      next();
     });
-    });
-    form.on("end", (project, field, empLogo)=>{
-       
-        console.log(field, empLogo); 
-        next();
-        
-    })
-}
+  });
+};
 
-module.exports = {uploadFile}
+module.exports = { uploadFile };
--- routes/employer.route.js
+++ routes/employer.route.js
@@ -4,8 +4,8 @@ const employerController = require("../controller/employer.controller");
 const dbConn = require("../middleware/dbConn.middle");
 const infoValidator = require("../middleware/infoValidation.middle");
 // const authMiddleware = require("../middleware/authMiddleware");
-const multer = require("multer");
-const uploadFileMiddleWare= require("../middleware/uploadFile.middleWare")
+
+const uploadFileMiddleWare = require("../middleware/uploadFile.middleWare");
 route.post(
   "/register",
   infoValidator.empEmailValidate,
@@ -19,33 +19,12 @@ route.post("/empNewFeedback", dbConn.conn, (req, res) => {
 
 route.post("/login", dbConn.conn, employerController.login);
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-      cb(null, './public/uploads');
-  },
-  filename: function (req, file, cb) {
-      cb(null, file.originalname);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
-  } else {
-      cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-      fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
-
-route.post("/updatedEmployerInfo", dbConn.conn, uploadFileMiddleWare.uploadFile , employerController.updatedInfo);
+route.post(
+  "/updatedEmployerInfo",
+  dbConn.conn,
+  uploadFileMiddleWare.uploadFile,
+  employerController.updatedInfo
+);
 
 route.put("/updatePassword", dbConn.conn, employerController.updatePassword);
 
@@ -55,6 +34,6 @@ route.put(
   "/empUpdateToken",
   dbConn.conn,
   employerController.updateTokenWithEmpId
-)
+);
 
 module.exports = route;
